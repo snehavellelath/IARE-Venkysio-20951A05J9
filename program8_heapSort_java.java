30c37f15cb41ea64c223943e3f447f339a47546b
vellsneha <118613754+vellsneha@users.noreply.github.com>
2023-03-17 23:42:58 +0530

    Create program8_heapSort_java.java

---
+++ program8_heapSort_java.java
@@ -0,0 +1,56 @@
+public class HeapSort {
+    public void sort(int arr[]) {
+        int n = arr.length;
+
+        // Build a max heap
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        // Extract elements from heap one by one
+        for (int i = n - 1; i >= 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // Call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+
+    void heapify(int arr[], int n, int i) {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
+
+        // If left child is larger than root
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        // If right child is larger than largest so far
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 4, 2, 8, 5, 1, 3, 6 };
+        HeapSort ob = new HeapSort();
+        ob.sort(arr);
+        System.out.println("Sorted array:");
+        for (int i = 0; i < arr.length; ++i)
+            System.out.print(arr[i] + " ");
+    }
+}
+
+
+
